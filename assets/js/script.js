f863f05e06452584d765e9362482a5ca7d797239
Code-Monster1 <usupovaliakbar01@gmail.com>
2023-03-14 00:42:47 +0600

    comments done

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,21 @@ var swiper = new Swiper("#swiper", {
     },
   },
 });
+
+var swiper2 = new Swiper("#swiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+    
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    920: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+  },
+});
